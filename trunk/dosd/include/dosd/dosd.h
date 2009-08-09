23693c13c60b834395233b3d21068b6bd282d337
scahoo <scahoo@af800e60-8326-11de-825f-1db895702d67>
2009-08-09 17:35:23 +0000

    Initial OSD import.

---
+++ trunk/dosd/include/dosd/dosd.h
@@ -0,0 +1,22 @@
+/*
+ *  Copyright (C) 2009 sca <scahoo <at> gmail <dot> com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+ 
+#ifndef __DOSD_H__
+#define __DOSD_H__
+
+#include <SDL.h>
+#include <stdint.h>
+
+#define DOSD_UPDATE_INTERVAL (500)
+#define DOSD_PADDING         (4)
+
+int dosd_init(uint32_t color);
+void dosd_deinit();
+void dosd_show(SDL_Surface *surface);
+
+#endif//__DOSD_H__
