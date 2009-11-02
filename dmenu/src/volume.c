8e6d6ae100241581dae1f006e93b3b93adaac70e
timothy.soehnlin <timothy.soehnlin@af800e60-8326-11de-825f-1db895702d67>
2009-11-02 14:01:47 +0000

    Adding changes to volume handling to ues absolute paths and new common utilities

--- dmenu/src/volume.c
+++ dmenu/src/volume.c
@@ -1,52 +1,30 @@
 #include "volume.h"
 
-#define ICON_x 253
-#define ICON_y 3
-#define ICON_w 9
-#define ICON_h 9
-
-#define TEXT_x 258
-#define TEXT_y -1
+extern TTF_Font* status_font;
+extern cfg_t *cfg_value;
 
 int base_volume;
 
 SDL_Surface* volume_status;
-SDL_Surface* tmp_surface;
 SDL_Color status_color = {255,255,255,0};
-
-extern TTF_Font* status_font;
-
 SDL_Rect dst_icon, dst_text;
 
-extern cfg_t *cfg_value;
-
 void vol_init() {
-
-	base_volume = (int)cfg_getint(cfg_value, "SndVol");
-
-	tmp_surface = IMG_Load("../../../home/.dmenu/STATspeaker.png");
-	if (tmp_surface == NULL) {
-		printf("Failed to load ../../../home/.dmenu/STATspeaker.png: %s\n", IMG_GetError());
-		exit(EXIT_FAILURE);
-	}
-	volume_status = SDL_DisplayFormatAlpha(tmp_surface);
-	SDL_FreeSurface(tmp_surface);
-
-	vol_set(0);
+        
+    base_volume = (int)cfg_getint(cfg_value, "SndVol");
+    volume_status = load_user_image("STATspeaker.png");
+    vol_set(0);
 
 }
 
 void vol_set(int change) {
-	char *mixer_device = "/dev/mixer";
 	int mixer;
 
-	base_volume += change;
-
-	if (base_volume >= 100) base_volume = 100;
-	if (base_volume <= 0) base_volume = 0;
+	base_volume = bound(base_volume + change, 0, 100);
 
 	int oss_volume = base_volume | (base_volume << 8); // set volume for both channels
-	mixer = open(mixer_device, O_WRONLY);
+    
+	mixer = open(MIXER_DEVICE, O_WRONLY);
 	if (ioctl(mixer, SOUND_MIXER_WRITE_VOLUME, &oss_volume) == -1) {
 		fprintf(stderr, "Failed opening mixer for write - VOLUME\n");
 	}
@@ -58,24 +36,21 @@ void vol_set(int change) {
 
 void vol_show(SDL_Surface* surface) {
 
+    SDL_Surface* volume_text;
 	char buff[10];
-	SDL_Surface* volume_text;
-
-	dst_icon.x = ICON_x;
-	dst_icon.y = ICON_y;
-	dst_icon.w = ICON_w;
-	dst_icon.h = ICON_h;
-
-	dst_text.x = TEXT_x;
-	dst_text.y = TEXT_y;
-
+    sprintf(buff, "%4d%%", base_volume);
+    
+	init_rect(&dst_icon, 
+        VOLUME_ICON_X, VOLUME_ICON_Y,
+        VOLUME_ICON_W, VOLUME_ICON_H);
+        
+    init_rect_pos(&dst_text, 
+        VOLUME_TEXT_X, VOLUME_TEXT_Y);
+    
 	SDL_BlitSurface( volume_status, NULL, surface, &dst_icon );
-	sprintf(buff, "%4d%%", base_volume);
-	tmp_surface = TTF_RenderUTF8_Solid( status_font, buff, status_color);
-	volume_text = SDL_DisplayFormatAlpha(tmp_surface);
-	SDL_BlitSurface( volume_text, NULL, surface, &dst_text );
-
-	SDL_FreeSurface(tmp_surface);
+    
+    volume_text = render_text(buff, status_font, &status_color, 1);
+	SDL_BlitSurface(volume_text, NULL, surface, &dst_text );
 	SDL_FreeSurface(volume_text);
 
 }
@@ -85,4 +60,3 @@ void vol_deinit() {
 	SDL_FreeSurface(volume_status);
 
 }
-
