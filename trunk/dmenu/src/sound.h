bbc815ebe1c640f1438732c7f963c5f30ba60faa
timothy.soehnlin <timothy.soehnlin@af800e60-8326-11de-825f-1db895702d67>
2009-11-02 14:00:57 +0000

    Adding changes to sound to ues absolute paths and new common utilities

--- trunk/dmenu/src/sound.h
+++ trunk/dmenu/src/sound.h
@@ -1,10 +1,5 @@
-#define MENU_MOVE	0
-#define MENUITEM_MOVE	1
-#define DECIDE		2
-#define CANCEL		3
-#define OUT		4
-#define TEST		5
+enum MenuSound { MENU_MOVE = 0, MENUITEM_MOVE = 1, DECIDE = 2, CANCEL = 3, OUT = 4, TEST = 5};
 
 void SE_Init();
-void SE_out(int seNum); 
+void SE_out(enum MenuSound seNum); 
 void SE_deInit();
