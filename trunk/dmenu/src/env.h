e9f19776e08481101949e0c2300df3a227d33fcd
timothy.soehnlin <timothy.soehnlin@af800e60-8326-11de-825f-1db895702d67>
2009-11-02 13:30:02 +0000

    Adding new header files for environment data, and global operations

---
+++ trunk/dmenu/src/env.h
@@ -0,0 +1,62 @@
+//Currently the hardcoded defaults are intended for Dingoo A320
+#ifndef DMENU_PATH
+#define DMENU_PATH           "/usr/local/dmenu/"
+#endif 
+#define DMENU_THEMES         DMENU_PATH "themes/"
+#define DMENU_CONF_FILE      DMENU_PATH "main.cfg"
+
+#ifndef HOME_PATH
+#define HOME_PATH            "/usr/local/home/"
+#endif
+
+#define USER_PATH            HOME_PATH ".dmenu/"
+#define USER_CONF_FILE       USER_PATH "dmenu.ini"
+#define USER_BACKGROUNDS     USER_PATH "bgimages/"
+
+//Device Info
+#define BACKLIGHT_DEVICE     "/proc/jz/lcd_backlight"
+#define MIXER_DEVICE         "/dev/mixer"
+#define BATTERY_DEVICE       "/proc/jz/battery"
+#define CHARGE_STATUS_DEVICE "/proc/jz/gpio1_pxpin"
+#define LOCK_STATUS_DEVICE   "/proc/jz/gpio3_pxpin"
+
+#define SCREEN_WIDTH         320
+#define SCREEN_HEIGHT        240
+#define SCREEN_COLOR_DEPTH   16
+#define SCREEN_BPP           (SCREEN_COLOR_DEPTH>>3)
+
+//Filelist options
+#define FILE_LIST_OFFSET     24
+#define FILE_ENTRY_HEIGHT    27
+#define FILES_PER_PAGE       ((SCREEN_HEIGHT - FILE_LIST_OFFSET)/FILE_ENTRY_HEIGHT)
+
+//Imageview options
+#define IMAGE_THUMB_RATIO_OUTER .25
+#define IMAGE_THUMB_RATIO_INNER .225
+#define IMAGE_PREVIEW_RATIO     .7
+#define IMAGE_THUMBS_PER_PAGE   (1/IMAGE_THUMB_RATIO_OUTER)
+#define IMAGE_THUMB_HEIGHT      (IMAGE_THUMB_RATIO_OUTER*SCREEN_HEIGHT)
+#define IMAGE_THUMB_WIDTH       (IMAGE_THUMB_RATIO_OUTER*SCREEN_WIDTH)
+#define IMAGE_THUMB_PAD_X       ((SCREEN_WIDTH*IMAGE_THUMB_RATIO_OUTER-SCREEN_WIDTH*IMAGE_THUMB_RATIO_INNER)/2.0)
+#define IMAGE_THUMB_PAD_Y       ((SCREEN_HEIGHT*IMAGE_THUMB_RATIO_OUTER-SCREEN_HEIGHT*IMAGE_THUMB_RATIO_INNER)/2.0)
+#define IMAGE_THUMB_TOP         (SCREEN_HEIGHT-IMAGE_THUMB_HEIGHT-2)
+
+//Volume Positioning
+#define VOLUME_ICON_X 253
+#define VOLUME_ICON_Y 3
+#define VOLUME_ICON_W 9
+#define VOLUME_ICON_H 9
+
+#define VOLUME_TEXT_X 258
+#define VOLUME_TEXT_Y -1
+
+//Brightness positioning
+#define BRIGHTNESS_ICON_X 238
+#define BRIGHTNESS_ICON_Y 3
+#define BRIGHTNESS_ICON_W 9
+#define BRIGHTNESS_ICON_H 9
+
+// the system ARG_MAX might be too large. just use a fixed
+// value here.
+#define MAX_CMD_LEN   4096
+#define TICK_INTERVAL 100 // this is 1000/100 = 10 fps
---
+++ trunk/dmenu/src/main.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+void deinit();
+void reload();
+void quick_exit();
+
+#endif
