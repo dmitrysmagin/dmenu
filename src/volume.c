932ef2557ed7926a5ad5667f369e66dfa8a854f4
timothy.soehnlin <timothy.soehnlin@af800e60-8326-11de-825f-1db895702d67>
2009-12-07 02:15:56 +0000

    Finished alpha blending for main menu and changed volume indication from text to an image that clips at the percentage of the current volume.

--- src/volume.c
+++ src/volume.c
@@ -10,7 +10,7 @@ SDL_Surface* volume_text;
 TTF_Font* volume_font;
 
 SDL_Color status_color = {DOSD_COLOR_RGBA};
-SDL_Rect vol_dst_icon, vol_dst_text;
+SDL_Rect vol_src_rect, vol_dst_rect;
 
 int vol_enabled()
 {
@@ -23,24 +23,26 @@ void vol_init() {
     
     base_volume = (int)cfg_getint(cfg_main, "SndVol");
     volume_status = load_global_image("STATspeaker.png");
-    volume_font = get_osd_font();
     
+    //Initialize OSD positions
+    init_rect(&vol_dst_rect, 
+        VOLUME_ICON_X, VOLUME_ICON_Y,
+        VOLUME_ICON_MAX_W, VOLUME_ICON_H);
+    
+    init_rect(&vol_src_rect, 0, 0,
+        VOLUME_ICON_MIN_W, VOLUME_ICON_H);
+              
     //Set volume
     vol_set(0);
-
-    //Initialize OSD positions
-    init_rect(&vol_dst_icon, 
-        VOLUME_ICON_X, VOLUME_ICON_Y,
-        VOLUME_ICON_W, VOLUME_ICON_H);
-    init_rect_pos(&vol_dst_text, 
-        VOLUME_TEXT_X, VOLUME_TEXT_Y);
 }
 
 void vol_set(int change) {
 	int mixer;
 
 	base_volume = bound(base_volume + change, 0, 100);
-    
+
+    vol_src_rect.w = VOLUME_ICON_MIN_W+(int)((VOLUME_ICON_MAX_W-VOLUME_ICON_MIN_W)*(base_volume/100.0f));
+              
 	int oss_volume = base_volume | (base_volume << 8); // set volume for both channels
     
 	mixer = open(MIXER_DEVICE, O_WRONLY);
@@ -50,30 +52,15 @@ void vol_set(int change) {
 	close(mixer);
 
     cfg_setint( cfg_main, "SndVol", (long)base_volume );
-    vol_set_text(base_volume);
-}
-
-void vol_set_text(int volume)
-{ 
-    char buff[10];
-    sprintf(buff, "%3d%%", volume);
-    
-    free_surface(volume_text);
-    volume_text = render_text(buff, volume_font, &status_color, 1);
-    
-    log_debug("Setting volume text: %3d%%", volume);
 }
 
 void vol_show(SDL_Surface* surface) 
 {    
-    SDL_BlitSurface(volume_status, NULL, surface, &vol_dst_icon );    
-    SDL_BlitSurface(volume_text,   NULL, surface, &vol_dst_text );
+    SDL_BlitSurface(volume_status, &vol_src_rect, surface, &vol_dst_rect );    
 }
 
 void vol_deinit() 
 {
     log_debug("De-initializing");
-    free_font(volume_font);
     free_surface(volume_status);
-    free_surface(volume_text);
 }
