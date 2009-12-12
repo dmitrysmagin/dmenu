4ec13298ca8ca0f8ff3a61ebc2d83b7a0d6c79fb
timothy.soehnlin <timothy.soehnlin@af800e60-8326-11de-825f-1db895702d67>
2009-12-12 13:38:10 +0000

    Finished up release script

--- trunk/scripts/release.sh
+++ trunk/scripts/release.sh
@@ -6,30 +6,33 @@ if ! scripts/build.sh; then
     exit
 fi
 
-
+BASE=`pwd -P`
 REL_ROOT=release
 REL_FULL=$REL_ROOT/local/dmenu
 
 #Clean up host files, only leave dingoo
 make -f Makefile.host clean
 
-#copy over basic file
+#Prep build area
+rm -rf $REL_FULL
 mkdir -p $REL_FULL
+
+#copy over basic file
 for x in changelog.txt readme.txt; do
-  cp $x $REL_ROOT
+  cp $BASE/$x $REL_ROOT
 done
 
 #Copy over global assets, executables
 for x in resources wallpapers dmenu*; do
-    cp -r $x $REL_FULL
+    cp -r $BASE/$x $REL_FULL
 done
 
 #Add default theme
 mkdir -p $REL_FULL/themes/default
-cp -r themes/default/* $REL_FULL/themes/default/
+cp -r $BASE/themes/default/* $REL_FULL/themes/default/
 
-rm -f release-$1.zip
+rm -f $BASE/release-$1.zip
 cd $REL_ROOT
-zip -r -x"*.svn*" -x"*.thumb*" -x"*-host*" ../release-$1.zip .
-cd -
+zip -r -x"*.svn*" -x"*.thumb*" -x"*-host*" $BASE/release-$1.zip .
+cd $BASE
 
