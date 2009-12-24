6cfc41c0f96605611305f21453385ac56fb360f5
leo.5xl.bigfat <leo.5xl.bigfat@af800e60-8326-11de-825f-1db895702d67>
2009-12-24 08:05:44 +0000

    Add Watch & Date display (default setting is false) . To use this, you need a special kernel which was distributed by BouKiCHi.
    http://clogging.web.fc2.com/dingoo/
    Linux kernel(dingux) for A320 (on megaupload.com)
    filesize : 2659234
    MD5 : ed6b9c1806ade92f11224efb2a80e094
    This kernel enables RTC and power off. 
    Note : RTC register will be destroyed by original firmware, 
    Linux SPL can be used to avoid this.
    
    BouKiCHi's blog (Japanese) : http://clogging.blog57.fc2.com/

---
+++ trunk/src/watch.c
@@ -0,0 +1,61 @@
+#include "watch.h"
+#include "resource.h"
+
+extern cfg_t *cfg_main;
+
+SDL_Rect watch_dst_rect1, watch_dst_rect2;
+TTF_Font* watch_font;
+// SDL_Color* watch_font_color;
+
+time_t watch;
+struct tm *t_st;
+
+int watch_enabled()
+{
+    return cfg_getbool(cfg_main, "WatchDisp");
+}
+
+void watch_init() 
+{    
+    log_debug("Initializing");
+    
+    //Initialize OSD positions
+    init_rect_pos( &watch_dst_rect1, WATCH_DISP_X, WATCH_DISP_Y);
+    init_rect_pos( &watch_dst_rect2, WATCH_DISP_X+1, WATCH_DISP_Y);
+	watch_font = get_theme_font(WATCH_FONT_SIZE);
+//	watch_font_color =get_theme_font_color();
+}
+
+void watch_show(SDL_Surface* surface) 
+{
+	char watch_text[20];
+	SDL_Surface* watch_display;
+	SDL_Color color_white = {255, 255, 255, 0};
+
+	time(&watch);
+	t_st = localtime(&watch);
+	if (t_st->tm_year < 1900 ) t_st->tm_year += 1900;
+
+	sprintf( watch_text, "%04d/%02d/%02d %02d:%02d:%02d",
+			t_st->tm_year,
+			t_st->tm_mon,
+			t_st->tm_mday,
+			t_st->tm_hour,
+			t_st->tm_min,
+			t_st->tm_sec);
+
+	watch_display = draw_text(watch_text, watch_font, &color_white);
+
+    SDL_BlitSurface(watch_display, 0, surface, &watch_dst_rect1 );
+    SDL_BlitSurface(watch_display, 0, surface, &watch_dst_rect2 );
+
+    free_surface(watch_display);
+}
+
+void watch_deinit() 
+{
+    log_debug("De-initializing");
+
+    free_font(watch_font);
+//    free_color(watch_font_color);
+}
